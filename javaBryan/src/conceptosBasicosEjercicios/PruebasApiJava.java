230a45f581a89090689b9f92c74753e86e993dc5
Bryan <bryan.dweb@gmail.com>
2024-12-16 20:11:10 +0100

    nie and pruebas

--- javaBryan/src/conceptosBasicosEjercicios/PruebasApiJava.java
+++ javaBryan/src/conceptosBasicosEjercicios/PruebasApiJava.java
@@ -1,23 +1,18 @@
 package conceptosBasicosEjercicios;
 
+import java.util.Arrays;
+
 public class PruebasApiJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String cadena = "89";
-
+		String cadena = "123";
 		int numero = 45;
-		
-		char ta='A';
-		
-		String t=Character.toString(ta);
-		
-		System.out.println(t);
 
-		System.out.println(String.valueOf(34) + 2);
-		
-		
+		char ta = 'A';
+		int[] arrayInt = { 1, 2, 3, 4 };
+		System.out.println(Arrays.toString(arrayInt));
 
 	}
 
