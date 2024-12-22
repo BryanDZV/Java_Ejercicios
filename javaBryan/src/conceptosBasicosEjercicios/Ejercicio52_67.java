8b92923f6daf43245a39b46c2e45663e479ce9a3
BryanDZV <bryan.dweb@gmail.com>
2024-12-22 13:51:40 +0100

    67

--- javaBryan/src/conceptosBasicosEjercicios/Ejercicio52_67.java
+++ javaBryan/src/conceptosBasicosEjercicios/Ejercicio52_67.java
@@ -1,10 +1,51 @@
 package conceptosBasicosEjercicios;
 
+import utilidades.Entrada;
+
 public class Ejercicio52_67 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("inserta una frase");
+		//String frase = Entrada.cadena();
+		String frase="pato lucas";
+
+		System.out.println("inserta una palabra para colocarla en medio de la frase");
+		//String palabra = Entrada.cadena();
+		String palabra="jose";
+		
+		System.out.println("resultado :::"+palabraEnMedio(frase,palabra));
+
+	}
+
+	private static String palabraEnMedio(String frase, String palabra) {
+		// TODO Auto-generated method stub
+		
+		//variables
+		
+		String resultado="";
+		
+		//pasar a array 
+		
+		String [] arrayFrase=frase.split(" ");
+		
+		//mita de la cadena
+		
+		int sizeMedio=(arrayFrase.length)/2;
+		System.out.println(sizeMedio);
+		
+		//insertar la palabra en medio en un array :for
+		
+		for (String string : arrayFrase) {
+			resultado+=string.substring(0);
+		}
+		
+		
+		
+		
+		
+		return resultado;
 	}
 
 }
