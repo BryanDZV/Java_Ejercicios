a7e5af4ef86e3779583533c6064e32951f6f343f
BryanDZV <bryan.dweb@gmail.com>
2025-02-06 19:01:11 +0100

    nuevo

--- soluciones.clase24-25/src/graficos/RepresentandoCartas.java
+++ soluciones.clase24-25/src/graficos/RepresentandoCartas.java
@@ -1,5 +1,6 @@
 package graficos;
 
+import java.util.List;
 import java.util.Random;
 
 import objetos.Carta;
@@ -8,23 +9,72 @@ import utilidades.StdDraw;
 
 public class RepresentandoCartas {
 
+	private static boolean recienPulsado;
+
 	public static void main(String[] args) {
 		// Establecer sistema de coordenadas
 		StdDraw.setXscale(-100, 100);
 		StdDraw.setYscale(-100, 100);
 		StdDraw.enableDoubleBuffering();
 		
-		Carta c=new Carta(Palo.OROS,12);
-		Carta c2=new Carta(Palo.BASTOS,7);
-	
+		int margenIzquierdo=-90;
+		int margenSuperior=70;
+		
+		List<Carta> baraja=Carta.crearBarajaEspaniola();
 		for (;;) {
 			StdDraw.clear();
-			c.dibujar(0,0,25,50);
-			c2.dibujar(75, 75, 12, 20);
+			for (int i = 0; i < baraja.size(); i++) {
+				int fila=0;
+				switch(baraja.get(i).getPalo()) {
+				case OROS:
+					fila=0;
+					break;
+				case COPAS:
+					fila=1;
+					break;
+				case ESPADAS:
+					fila=2;
+					break;
+				case BASTOS:
+					fila=3;
+					break;
+				}
+				//Convierto numero de la carte en un valor del 0 al 9
+				int numero0a9=baraja.get(i).getNumero()-1;
+				if (numero0a9>6)
+					numero0a9=numero0a9-2;
+				baraja.get(i).dibujar(margenIzquierdo+numero0a9*20, margenSuperior-fila*35, 20, 30);
+			}
 			
-			StdDraw.show();
-			StdDraw.pause(100);
+			if (clickRaton()) {
+				try {
+					List <Carta> mano=Carta.extraerMano(baraja, 7);
+					System.out.println(mano);
+				} catch (RuntimeException e) {
+					System.out.println("No hay más cartas.");					
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			
+			
+StdDraw.show
+();
+			StdDraw.pause(50);
 		}
+		
+		
 
 	}
-}
+	
+	private static boolean clickRaton() {
+		if (StdDraw.isMousePressed()) {
+			if (!recienPulsado) {
+				recienPulsado = true;
+				return true;
+			}
+		}else
+			recienPulsado = false;
+		return false;
+	}
+} 
