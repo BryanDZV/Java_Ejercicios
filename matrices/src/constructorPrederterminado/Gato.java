da78ae6be46fb26c817488da04d666e2686e91cb
BryanDZV <bryan.dweb@gmail.com>
2025-02-13 18:49:31 +0100

    matrices proyecto

---
+++ matrices/src/constructorPrederterminado/Gato.java
@@ -0,0 +1,30 @@
+package constructorPrederterminado;
+
+public class Gato {
+	
+	String nombre="desconocido";
+	int edad=0;
+	
+	
+	
+	
+	
+	
+	public Gato(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		//Gato p=new Gato();
+		
+	}
+
+
+	
+	
+
+}
--- soluciones.clase24-25/src/objetos/Palo.java
+++
@@ -1,5 +0,0 @@
-package objetos;
-
-public enum Palo {
-	OROS,COPAS,ESPADAS,BASTOS
-}
