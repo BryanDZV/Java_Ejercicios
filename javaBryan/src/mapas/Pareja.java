c3f1329440773233cad32fe489804f14d9a15cfc
BryanDZV <bryan.dweb@gmail.com>
2025-03-17 12:34:34 +0100

    volcadoListaTEcnica

---
+++ javaBryan/src/mapas/Pareja.java
@@ -0,0 +1,26 @@
+package mapas;
+
+public class Pareja {
+	String palEs;
+	String palIng;
+	
+	public Pareja(String palEs, String palIng) {
+		super();
+		this.palEs = palEs;
+		this.palIng = palIng;
+	}
+	
+	public String getPalEs() {
+		return palEs;
+	}
+	public void setPalEs(String palEs) {
+		this.palEs = palEs;
+	}
+	public String getPalIng() {
+		return palIng;
+	}
+	public void setPalIng(String palIng) {
+		this.palIng = palIng;
+	}
+
+}
