1c33f1cb0d896f75110bc7a0cc1c0eb7ce41cc5a
Bryan <bryan.dweb@gmail.com>
2025-03-25 23:22:43 +0100

    FICHEROS

--- javaBryan/src/ficheros/Ejercicio3.java
+++ javaBryan/src/ficheros/Ejercicio3.java
@@ -6,15 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Ejercicio3 {
+	/*
+	 * En Windows, puedes usar \\ para las barras invertidas (\) o / para las barras
+	 * diagonales (/).
+	 * 
+	 * En Unix/Linux, es común usar solo /.
+	 */
 
 	public static void main(String[] args) throws IOException {
-		File f=new File("C:\\Users\\bryan\\Desktop\\ies grado superior\\programacion\\3EVAPROGRAMACION\\pruebasFicheros");
-		BufferedReader bfr=new BufferedReader(new FileReader(f));
-		String linea=bfr.readLine();
+		File f = new File(
+				"I:\\Otros ordenadores\\Mi portátil hp\\ies grado superior\\programacion\\3EVAPROGRAMACION\\pruebasFicheros\\pruebaProg.csv");
+		BufferedReader bfr = new BufferedReader(new FileReader(f));
+		String linea = bfr.readLine();
 		do {
-			System.out.println(linea+" ");
-			linea=bfr.readLine();
-		} while (linea!=null);
+			System.out.println(linea + " ");
+			linea = bfr.readLine();
+		} while (linea != null);
 	}
 
 }
